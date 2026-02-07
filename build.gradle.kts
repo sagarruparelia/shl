9befefcc0e5edec5858cc86dead5d26b1d297f7f
Sagar <ruparelia.sagar@gmail.com>
2026-02-07 16:25:38 -0500

    Group dependencies by their destination.

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,35 +29,31 @@ val zxingVersion = "3.5.4"
 val awsSdkVersion = "2.41.23"
 
 dependencies {
+    implementation(platform("software.amazon.awssdk:bom:$awsSdkVersion"))
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-json")
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    annotationProcessor("org.projectlombok:lombok")
-
-    // JWE encryption (alg:dir, enc:A256GCM) per SHL spec
-    implementation("com.nimbusds:nimbus-jose-jwt:$nimbusJoseJwtVersion")
-
-    // QR code generation (error correction level M)
-    implementation("com.google.zxing:core:$zxingVersion")
-    implementation("com.google.zxing:javase:$zxingVersion")
-
-    // AWS S3 (reactive) for encrypted payload + QR code storage
-    implementation(platform("software.amazon.awssdk:bom:$awsSdkVersion"))
-    implementation("software.amazon.awssdk:s3")
-    implementation("software.amazon.awssdk:auth")
-    implementation("software.amazon.awssdk:sts")              // IRSA support in EKS
-    implementation("software.amazon.awssdk:http-auth-aws")     // AwsV4HttpSigner (replaces deprecated Aws4Signer)
-    implementation("software.amazon.awssdk:netty-nio-client")
-
-    // Bean validation, passcode hashing, config metadata
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.security:spring-security-crypto")
+    implementation("com.nimbusds:nimbus-jose-jwt:$nimbusJoseJwtVersion")       // JWE encryption (alg:dir, enc:A256GCM)
+    implementation("com.google.zxing:core:$zxingVersion")                      // QR code generation
+    implementation("com.google.zxing:javase:$zxingVersion")
+    implementation("software.amazon.awssdk:s3")                                // AWS S3 (reactive)
+    implementation("software.amazon.awssdk:auth")
+    implementation("software.amazon.awssdk:sts")                               // IRSA support in EKS
+    implementation("software.amazon.awssdk:http-auth-aws")                     // AwsV4HttpSigner
+    implementation("software.amazon.awssdk:netty-nio-client")
+
+    compileOnly("org.projectlombok:lombok")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    annotationProcessor("org.projectlombok:lombok")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     testImplementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
